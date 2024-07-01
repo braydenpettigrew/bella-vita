c16975cf5e1df077369eb04800585824c4f1696e
Brayden Pettigrew <blp5473@psu.edu>
2024-07-01 13:35:57 -0400

    reset the http.js file

--- util/http.js
+++ util/http.js
@@ -100,3 +100,83 @@ export async function deleteHistory(timestamp, auth) {
     throw error;
   }
 }
+
+export async function storePushToken(pushToken, auth) {
+  const response = await axios.post(
+    `${BACKEND_URL}/pushtokens.json?auth=${auth}`,
+    pushToken
+  );
+  return response;
+}
+
+export async function storePushTokenWithEmail(pushToken, email, auth) {
+  // Check if push token with email already exists.
+  try {
+    const response = await axios.get(
+      `${BACKEND_URL}/pushtokens.json?auth=${auth}`
+    );
+
+    for (const key in response.data) {
+      if (response.data.hasOwnProperty(key)) {
+        const currentPushToken = response.data[key].pushToken;
+        const currentEmail = response.data[key].email;
+        if (currentPushToken === pushToken && currentEmail === email) {
+          return;
+        }
+      }
+    }
+    // If it does not exist, store it.
+    const response2 = await axios.post(
+      `${BACKEND_URL}/pushtokens.json?auth=${auth}`,
+      { pushToken: pushToken, email: email }
+    );
+    return response2;
+  } catch (error) {
+    console.log("http1 Error: ", error);
+    return error;
+  }
+}
+
+// Check if a specific push token exists on the backend.
+export async function pushTokenExists(pushToken, auth) {
+  try {
+    const response = await axios.get(
+      `${BACKEND_URL}/pushtokens.json?auth=${auth}`
+    );
+
+    for (const key in response.data) {
+      if (response.data.hasOwnProperty(key)) {
+        const currentPushToken = response.data[key].pushToken;
+        if (currentPushToken === pushToken) {
+          return true;
+        }
+      }
+    }
+    return false;
+  } catch (error) {
+    console.log("http1 Error: ", error);
+    return error;
+  }
+}
+
+// Return a list of all push tokens that are stored on the backend.
+export async function getAllPushTokens(auth) {
+  try {
+    const response = await axios.get(
+      `${BACKEND_URL}/pushtokens.json?auth=${auth}`
+    );
+
+    const pushTokens = [];
+
+    for (const key in response.data) {
+      if (response.data.hasOwnProperty(key)) {
+        pushTokens.push(response.data[key]);
+      }
+    }
+
+    return pushTokens;
+  } catch (error) {
+    console.log("http2 Error: ", error);
+    return error;
+  }
+}
